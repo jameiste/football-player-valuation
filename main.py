804ba519924291891c9b7693d9c84ab400a1288e
Jannis <jannismeister@gmx.de>
2026-01-16 00:04:48 +0100

    Fix: make direcotry if data folder non-existing

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# Runner for the app
-
+### Runner for the app ###
+# Imports
+import os
 # Local imports
 from backend.combine_data import data_table
 from backend.metric_analyzation.scoring import run_scoring
+from environment.variable import DATA_PATH
 
+# Make the data directory if not existing
+if not DATA_PATH.exists():
+    os.mkdir(DATA_PATH)
+    
 # Just run what is present now
 data = data_table()
 score = run_scoring()
